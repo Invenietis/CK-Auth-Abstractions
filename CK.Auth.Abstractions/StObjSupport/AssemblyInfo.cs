d10195c2a7eec5caa6ff11eb5283309546432906
Olivier Spinelli <olivier.spinelli@invenietis.com>
2018-10-12 18:33:12 +0200

    Using new IsModelDependent attribute.

--- CK.Auth.Abstractions/StObjSupport/AssemblyInfo.cs
+++ CK.Auth.Abstractions/StObjSupport/AssemblyInfo.cs
@@ -4,5 +4,5 @@ using System;
 // This enables the StdAuthenticationTypeSystem that is a ISingletonAmbientService to be handled by
 // CK.StObj automatic DI.
 //
-[assembly: CK.Setup.IsModel()]
-namespace CK.Setup { class IsModelAttribute : Attribute { } }
+[assembly: CK.Setup.IsModelDependent()]
+namespace CK.Setup { class IsModelDependentAttribute : Attribute { } }
