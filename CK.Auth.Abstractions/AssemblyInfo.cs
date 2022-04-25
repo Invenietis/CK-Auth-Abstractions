ab736952838224dfd8f4e0a90402ff7180cd694b
Olivier Spinelli <olivier.spinelli@signature-code.com>
2022-04-25 17:53:20 +0200

    Adding a dependency on CK.Core.

--- CK.Auth.Abstractions/AssemblyInfo.cs
+++ CK.Auth.Abstractions/AssemblyInfo.cs
@@ -3,6 +3,4 @@ using System;
 //
 // This enables the StdAuthenticationTypeSystem that is a ISingletonAutoService to be handled by
 // CK.StObj automatic DI.
-//
 [assembly: CK.Setup.IsModelDependent()]
-namespace CK.Setup { class IsModelDependentAttribute : Attribute { } }
