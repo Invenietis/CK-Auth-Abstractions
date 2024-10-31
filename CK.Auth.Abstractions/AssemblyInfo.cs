5242d981bdac747620e7bc591ab2ce153525cca2
Olivier Spinelli <olivier.spinelli@signature-code.com>
2024-10-31 19:44:03 +0100

    Fix Setup attributes.

--- CK.Auth.Abstractions/AssemblyInfo.cs
+++
@@ -1,5 +0,0 @@
-
-//
-// This enables the StdAuthenticationTypeSystem that is a ISingletonAutoService to be handled by
-// CK.StObj automatic DI (and the IAuthenticationInfo to be a ubiquitous endpoint scoped service).
-[assembly: CK.Setup.IsModelDependent()]
